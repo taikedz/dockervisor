1200f98a02e6514e2b8e980b34880f1aefd475ce
Tai Kedzierski <dch tai gmail>
2017-10-06 22:32:42 +0100

    add install script

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -eu
+
+cd "$(dirname "$0")"
+
+[[ "$UID" = 0 ]] || {
+	echo "You must be root to run this script"
+	exit 1
+}
+
+DOCKERVISOR_DIR="/usr/share/dockervisor"
+DOCKERVISOR_EXE="/usr/bin/dockervisor"
+
+if [[ -d "$DOCKERVISOR_DIR" ]]; then
+	echo "Removing existing dockervisor ..."
+	rm -r "$DOCKERVISOR_DIR"
+fi
+
+echo "Copying dockervisor to $DOCKERVISOR_DIR..."
+cp -r dockervisor "$DOCKERVISOR_DIR"
+
+if [[ -h "$DOCKERVISOR_EXE" ]]; then
+	echo "Unlinking old dockervisor command ..."
+	unlink "$DOCKERVISOR_EXE"
+fi
+
+echo "Installing new dockervisor ..."
+ln -s "$DOCKERVISOR_DIR/runtime.py" "$DOCKERVISOR_EXE"
+
+echo "Finished."
