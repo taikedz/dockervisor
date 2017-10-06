c5072b71943acc400648f78729bb364c73dda4b1
Tai Kedzierski <dch tai gmail>
2017-10-06 21:23:36 +0100

    remove guff from previous usage

--- dockervisor/run.py
+++ dockervisor/run.py
@@ -9,12 +9,6 @@ Command runner
 Issues commands in the shell
 """
 
-def execute(command_tokens):
-    """ Runs command (first token), replacing current process
-    """
-    print ' '.join(command_tokens)
-    os.execvp(command_tokens[0], command_tokens)
-
 def call(command_tokens):
     """ Runs a command (first token), and resumes python script
 
