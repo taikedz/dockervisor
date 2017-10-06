3caa78a11b07222e7576a6af76d6fbdcd7a44087
Tai Kedzierski <dch.tai@gmail.com>
2017-10-06 17:31:44 +0100

    stability marking

---
+++ dockervisor/stability.py
@@ -0,0 +1,20 @@
+from dockervisor import store
+from dockervisor import common
+from dockervisor import container
+
+# dockervisor stable IMAGE
+
+def stable(args):
+    if not common.args_check(args, 1):
+        common.fail("Insufficient arguments. Run 'docker stable IMAGE' to mark IMAGE's currently running container as stable")
+    imagename = args[0]
+
+    containernames = container.get_container_names(imagename)
+
+    if len(containernames) != 1:
+        print(containernames)
+        common.fail("Could not identify single container to mark as stable")
+
+    containername = containernames[0]
+
+    store.registry_set_instance("stable", imagename, containername)
