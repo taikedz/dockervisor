687dadd500899be7e01b595fe5f0fb075b4be0f9
Tai Kedzierski <dch tai gmail>
2017-10-08 19:42:18 +0100

    implementation notes

--- dockervisor/removal.py
+++ dockervisor/removal.py
@@ -1,7 +1,18 @@
 from dockervisor import common
+from dockervisor import listing
 
-# dockervisor remove olderthan DATESTRING
-# dockervisor remove all IMAGENAME
+# dockervisor remove IMAGENAME
 
 def remove(args):
+    if not common.args_check(args,1):
+        common.fail("Usage:\n    dockervisor remove IMAGENAME")
+
+    imagename = args[0]
+    images = listing.get_image_list_for(imagename)
+
+    # TODO remove all containers
+    # remove all images
+
+# remove all old containers and images (not current) (not stable)
+def clean(args):
     pass
