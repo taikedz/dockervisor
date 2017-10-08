8fd99cfde7a79997e293747e1ae130040815102c
Tai Kedzierski <dch tai gmail>
2017-10-08 19:32:27 +0100

    allow attaching to containers

---
+++ dockervisor/attach.py
@@ -0,0 +1,21 @@
+from dockervisor import container
+from dockervisor import common
+import os
+
+def attach(args):
+    if not common.args_check(args, 1):
+        common.fail("Usage:\n  dockervisor attach IMAGENAME [COMMAND]")
+
+    imagename = args[0]
+
+    shellname = "bash"
+    if common.args_check(args, 2):
+        shellname = args[1]
+
+    containernames = container.get_running_containers(imagename)
+
+    if len(containernames) != 1:
+        common.fail("Could not identify single container to attach to:\n%s"%(str(containernames) ))
+
+    command_tokens = ["docker", "exec", "-it", containernames[0], shellname]
+    os.execvp(command_tokens[0], command_tokens)
