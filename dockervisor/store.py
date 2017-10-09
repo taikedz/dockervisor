257926c00ed02de1570b5e8ab5cfc3e457fe5391
Tai Kedzierski <dch.tai@gmail.com>
2017-10-09 11:04:29 +0100

    add an append_data function

--- dockervisor/store.py
+++ dockervisor/store.py
@@ -1,4 +1,5 @@
 import pathlib
+import os
 from dockervisor import files
 
 # Store dir structure -------------------------
@@ -21,14 +22,27 @@ def write_store_file(a_storefile, filedata):
     files.write_file(a_storefile, filedata)
 
 def write_data(filename, imagename, filedata):
+    ''' Writes text data to file, overwriting existing data
+    '''
     write_store_file(a_store_dir + [imagename, filename] , filedata )
 
+def append_data(filename, imagename, filedata):
+    ''' Writes new text data to the end of the file, after a new line
+    '''
+    olddata = read_data(filename, imagename)
+    if olddata:
+        filedata = os.linesep.join([olddata,filedata])
+
+    write_data(filename, imagename, filedata )
+
 def read_store_file(a_storefile):
-    ''' Returns the file's dfata, or none if the file is not found
+    ''' Returns the file's data, or None if the file is not found
     '''
     return files.read_file(a_storefile)
 
 def read_data(filename, imagename):
+    ''' Read text data from file; returns string data, or None if file was not found
+    '''
     return read_store_file(a_store_dir + [imagename, filename])
     
 
