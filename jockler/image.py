495143593195da6b87321e0f01ba5b4699247f95
Tai Kedzierski <dch.tai@gmail.com>
2017-10-09 19:49:28 +0100

    support alternative dockerfile names

--- jockler/image.py
+++ jockler/image.py
@@ -13,18 +13,30 @@ def build(args):
     if not re.match("^[a-zA-Z0-9]+$", imagename):
         common.fail("Image name can only have letters a-z, A-Z and numbers 0-9.")
 
-    build_path = common.item(args, 1, ".")
+    dockerfile, build_path = dockerfiles_for(imagename)
 
     # Add jcl file first - if cannot be generated, prevents build
     add_jcl(imagename)
 
-    res,sout,serr = do_build( imagename, build_path )
+    res,sout,serr = do_build( imagename, dockerfile, build_path )
 
     if res == 0:
         image_id = get_tagged_image_id(imagename)
         append_image_id(imagename, image_id)
 
     exit(res)
+
+def dockerfiles_for(imagename):
+    dockerfile = "%s-Dockerfile"%imagename
+    build_path = "%s-data"
+
+    if not os.path.isfile(dockerfile):
+        dockerfile = "Dockerfile"
+
+    if not os.path.isdir(build_path):
+        build_path = "."
+
+    return dockerfile, build_path
 
 def add_jcl(imagename):
     try:
@@ -51,7 +63,7 @@ def append_image_id(imagename, image_id):
 
     store.write_data( "images", imagename, os.linesep.join(imagelist) )
 
-def do_build(imagename, build_path):
-    print("Building [%s] at [%s] taking files from [%s]" % (imagename, os.path.realpath("Dockerfile"), build_path))
+def do_build(imagename, dockerfile, build_path):
+    print("Building [%s] at [%s] taking files from [%s]" % (imagename, os.path.realpath(dockerfile), build_path))
     return run.call(["docker", "build", "-t", imagename, build_path] , stdout=sys.stdout, stderr=sys.stderr)
 
