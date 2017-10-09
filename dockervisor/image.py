c2a00a46dd5f814790f2442f046f8a28e6f7b612
Tai Kedzierski <dch.tai@gmail.com>
2017-10-09 11:05:02 +0100

    write image id to file

--- dockervisor/image.py
+++ dockervisor/image.py
@@ -19,6 +19,11 @@ def build(args):
     add_dcv(imagename)
 
     res,sout,serr = do_build( imagename, build_path )
+
+    if res == 0:
+        image_id = get_tagged_image_id(imagename)
+        store.append_data("images", imagename, image_id)
+
     exit(res)
 
 def add_dcv(imagename):
@@ -26,6 +31,13 @@ def add_dcv(imagename):
         dockerfile.add_dcv_file(imagename)
     except json.JSONDecodeError as e:
         common.fail("Invalid dcv-%s file data:\n%s"%(imagename, str(e)))
+
+def get_tagged_image_id(imagename):
+    # This should perform an exact match
+    res,sout,serr = run.call(["docker", "images", "--format", "{{.ID}}", imagename], silent=True)
+
+    image_id = sout.strip()
+    return image_id
 
 def do_build(imagename, build_path):
     print("Building [%s] at [%s] taking files from [%s]" % (imagename, os.path.realpath("Dockerfile"), build_path))
