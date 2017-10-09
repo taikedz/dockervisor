ca87e0fd0171330139dd8b3a290a3cbf35f8aef2
Tai Kedzierski <dch.tai@gmail.com>
2017-10-09 17:24:27 +0100

    add stability marking help and show command

--- dockervisor/stability.py
+++ dockervisor/stability.py
@@ -4,10 +4,25 @@ from dockervisor import container
 
 # dockervisor stable IMAGE
 
-def stable(args):
-    if not common.args_check(args, 1):
-        common.fail("Insufficient arguments. Run 'docker stable IMAGE' to mark IMAGE's currently running container as stable")
-    imagename = args[0]
+helpstr = """
+Mark IMAGENAME's currently running container as stable:
+
+    dockervisor stable IMAGENAME
+
+See stable image:
+
+    dockervisor stable :show IMAGENAME
+
+"""
+
+def print_stable_container(imagename):
+    containername = store.read_data("stable", imagename)
+    if containername:
+        print(containername)
+    else:
+        print("No stable container marked for image [%s]"%imagename)
+
+def mark_stable_container(imagename):
 
     containernames = container.get_running_containers(imagename)
 
@@ -18,3 +33,16 @@ def stable(args):
     containername = containernames[0]
 
     store.write_data("stable", imagename, containername)
+
+def stable(args):
+
+    if not common.args_check(args, 1):
+        common.fail(helpstr)
+        
+    imagename = args[0]
+
+    if common.args_check(args, 2) and imagename == ":show":
+        imagename = args[1]
+        print_stable_container(imagename)
+    else:
+        mark_stable_container(imagename)
